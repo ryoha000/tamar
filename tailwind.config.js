a5964bf37b9f4037c54f599fe2fec7638f75e351
ryoha <the7uya@gmail.com>
2022-08-19 15:12:43 +0900

    fix config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,25 @@
 module.exports = {
   content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}",
   ],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
     extend: {
-    colors: {
-      background: 'var(--color-background)',
-      primary: 'var(--color-primary)',
-      secondary: 'var(--color-secondary)',
-      accent: 'var(--color-accent)',
-      error: 'var(--color-error)',
-      white: 'var(--color-white)',
-      text: 'var(--color-text)',
+      colors: {
+        background: "var(--color-background)",
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        accent: "var(--color-accent)",
+        error: "var(--color-error)",
+        white: "var(--color-white)",
+        text: "var(--color-text)",
+      },
+      zIndex: {
+        "entire-overlay": 100,
+        popup: 105,
+      },
     },
-    zIndex: {
-      'entire-overlay': 100,
-      'popup': 105,
-    },},
   },
   plugins: [],
 };

3899faaa0549c51ca4e0c4c87dc1aa260e02203e
ryoha <the7uya@gmail.com>
2022-08-19 17:47:20 +0900

    impl dialog

---
+++ src/components/UI/Dialog.tsx
@@ -0,0 +1,52 @@
+import { createEffect, onMount, ParentComponent, Show } from "solid-js";
+import { Portal } from "solid-js/web";
+import EntireOverlayCloser from "./EntireOverlayCloser";
+
+interface Props {
+  isOpen: boolean;
+  close: () => void;
+}
+
+const Dialog: ParentComponent<Props> = (props) => {
+  return (
+    <Show when={props.isOpen}>
+      <Portal>
+        <DialogContent isOpen={props.isOpen} close={props.close}>
+          {props.children}
+        </DialogContent>
+      </Portal>
+    </Show>
+  );
+};
+
+const DialogContent: ParentComponent<Props> = (props) => {
+  let ele: HTMLDivElement | undefined = undefined;
+  onMount(() => {
+    ele?.focus();
+  });
+
+  return (
+    <div
+      class="absolute top-0 bg-black bg-opacity-20 left-0 w-full h-full z-dialog"
+      tabIndex={-1}
+      onkeydown={(e) => {
+        if (e.key === "Escape") {
+          props.close();
+        }
+      }}
+      onclick={props.close}
+      ref={ele}
+    >
+      <div class="flex items-center justify-center px-12 py-8 h-full">
+        <div
+          onclick={(e) => e.stopPropagation()}
+          class="bg-white rounded px-12 py-8 shadow-md max-h-full overflow-y-scroll"
+        >
+          {props.children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dialog;

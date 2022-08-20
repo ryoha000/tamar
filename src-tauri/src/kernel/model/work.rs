05349e626c4f53b9191e0cf8c4cb442a49b906c8
ryoha <the7uya@gmail.com>
2022-08-21 03:00:15 +0900

    fix work definition in kernel

--- src-tauri/src/kernel/model/work.rs
+++ src-tauri/src/kernel/model/work.rs
@@ -5,7 +5,7 @@ use derive_new::new;
 #[derive(new, Debug)]
 pub struct Work {
     pub id: Id<Work>,
-    pub name: String,
+    pub title: String,
     pub artist_id: Id<Artist>,
     pub created_at: DateTime<Local>,
     pub updated_at: DateTime<Local>,
@@ -14,6 +14,6 @@ pub struct Work {
 #[derive(new, Debug)]
 pub struct NewWork {
     pub id: Id<Work>,
-    pub name: String,
+    pub title: String,
     pub artist_id: Id<Artist>,
 }

9888f2820eb6efcff93a2e62e736b69ef2913385
ryoha <the7uya@gmail.com>
2022-08-21 01:04:58 +0900

    fix migration

--- src-tauri/src/migration.rs
+++ src-tauri/src/migration.rs
@@ -5,14 +5,24 @@ pub async fn migration() {
     let db = Db::new().await;
     let pool = db.0.clone();
 
-    let sql = "
+    let sqls = get_migration_sqls();
+    for sql in sqls.iter() {
+        sqlx::query(sql).execute(&*pool).await.unwrap();
+    }
+
+    println!("end migration");
+}
+
+fn get_migration_sqls() -> Vec<String> {
+    let artist = "
 CREATE TABLE IF NOT EXISTS artist (
 	id varchar(36) primary key,
 	name varchar(255) not null,
 	created_at datetime not null,
 	updated_at datetime not null
 );
-    ";
-    sqlx::query(sql).execute(&*pool).await.unwrap();
-    println!("end migration");
+    "
+    .to_string();
+
+    return vec![artist];
 }

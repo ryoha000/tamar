91f902f0e9d86814f4f89cadfe7997cd5aa75ca6
ryoha <the7uya@gmail.com>
2022-08-21 02:15:29 +0900

    add create table work sql

--- src-tauri/src/migration.rs
+++ src-tauri/src/migration.rs
@@ -5,7 +5,7 @@ pub async fn migration() {
     let db = Db::new().await;
     let pool = db.0.clone();
 
-    let sqls = get_migration_sqls();
+    let sqls = get_create_table_sqls();
     for sql in sqls.iter() {
         sqlx::query(sql).execute(&*pool).await.unwrap();
     }
@@ -13,7 +13,7 @@ pub async fn migration() {
     println!("end migration");
 }
 
-fn get_migration_sqls() -> Vec<String> {
+fn get_create_table_sqls() -> Vec<String> {
     let artist = "
 CREATE TABLE IF NOT EXISTS artist (
 	id varchar(36) primary key,
@@ -24,5 +24,16 @@ CREATE TABLE IF NOT EXISTS artist (
     "
     .to_string();
 
-    return vec![artist];
+    let work = "
+CREATE TABLE IF NOT EXISTS work (
+	id varchar(36) primary key,
+	title varchar(255) not null,
+    artist_id varchar(36) not null,
+	created_at datetime not null,
+	updated_at datetime not null
+);
+    "
+    .to_string();
+
+    return vec![artist, work];
 }

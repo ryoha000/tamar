f66c8dbdfec3526cc14fb81e328bf4dd2108ce39
ryoha <the7uya@gmail.com>
2022-08-22 08:59:15 +0900

    fix

--- src/components/TopPage/SortSelect.tsx
+++ src/components/TopPage/SortSelect.tsx
@@ -4,9 +4,12 @@ import ToggleIconButton from "../UI/ToggleIconButton";
 import TooltipWrapper from "../UI/TooltipWrapper";
 import DropDownMenu from "../UI/DropDownMenu";
 
-const INITIAL_SELECT_OPTION = "追加日時";
+export const SORT_KIND = ["追加日時", "作品名", "閲覧日時"] as const;
+const INITIAL_SELECT_OPTION = SORT_KIND[0];
+export type SortKind = typeof SORT_KIND[number];
+
 const SortSelect: Component = () => {
-  const [selectedOption, setSelectedOption] = createSignal(
+  const [selectedOption, setSelectedOption] = createSignal<string>(
     INITIAL_SELECT_OPTION
   );
 
@@ -15,7 +18,7 @@ const SortSelect: Component = () => {
   return (
     <div class="flex items-center gap-1">
       <DropDownMenu
-        options={["追加日時", "作品名", "閲覧日時"]}
+        options={[...SORT_KIND]}
         onChange={(option) => setSelectedOption(option)}
         selectedOption={selectedOption()}
       />

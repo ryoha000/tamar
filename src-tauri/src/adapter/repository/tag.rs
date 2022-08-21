ecd4ab6348b329c457082fbe15e764a08df61f89
ryoha <the7uya@gmail.com>
2022-08-21 09:47:40 +0900

    impl find_by_name in tag

--- src-tauri/src/adapter/repository/tag.rs
+++ src-tauri/src/adapter/repository/tag.rs
@@ -26,6 +26,19 @@ impl TagRepository for DatabaseRepositoryImpl<Tag> {
         }
     }
 
+    async fn find_by_name(&self, name: String) -> anyhow::Result<Option<Tag>> {
+        let pool = self.pool.0.clone();
+        let tag_table = query_as::<_, TagTable>("select * from tag where name = ?")
+            .bind(name)
+            .fetch_one(&*pool)
+            .await
+            .ok();
+        match tag_table {
+            Some(st) => Ok(Some(st.try_into()?)),
+            None => Ok(None),
+        }
+    }
+
     async fn insert(&self, source: NewTag) -> anyhow::Result<()> {
         let pool = self.pool.0.clone();
         let tag_table: TagTable = source.try_into()?;
@@ -43,7 +56,7 @@ impl TagRepository for DatabaseRepositoryImpl<Tag> {
 
 #[cfg(test)]
 mod test {
-    use crate::kernel::model::tag::NewTag;
+    use crate::kernel::model::tag::{NewTag, Tag};
     use crate::kernel::model::Id;
     use crate::kernel::repository::tag::TagRepository;
     use crate::test_util::random_string;
@@ -57,13 +70,48 @@ mod test {
     #[test]
     fn test_insert_tag() {
         let db = block_on(Db::new());
-        let repository = DatabaseRepositoryImpl::new(db);
         let id = Ulid::new();
         let name = random_string();
-        let _ = block_on(repository.insert(NewTag::new(Id::new(id), name.clone()))).unwrap();
-        let found = block_on(repository.find(&Id::new(id))).unwrap().unwrap();
+
+        insert_tag(db.clone(), NewTag::new(Id::new(id), name.to_string()));
+        let found = find_tag(db, Id::new(id)).unwrap();
 
         assert_eq!(found.id.value, id);
-        assert_eq!(found.name, name);
+        assert_eq!(found.name, name.to_string());
+    }
+
+    #[test]
+    fn test_find_tag_by_id() {
+        let db = block_on(Db::new());
+        let id = Ulid::new();
+        let name = random_string();
+
+        insert_tag(db.clone(), NewTag::new(Id::new(id), name.to_string()));
+        let found = find_tag_by_name(db, name.to_string()).unwrap();
+        assert_eq!(found.id.value, id);
+        assert_eq!(found.name, name.to_string());
+    }
+
+    #[test]
+    fn test_find_tag_by_id_not_found() {
+        let db = block_on(Db::new());
+
+        let found = find_tag_by_name(db, "りょは9999999".to_string());
+        assert!(found.is_none());
+    }
+
+    fn insert_tag(db: Db, new_tag: NewTag) {
+        let repository = DatabaseRepositoryImpl::new(db);
+        block_on(repository.insert(new_tag)).unwrap()
+    }
+
+    fn find_tag(db: Db, id: Id<Tag>) -> Option<Tag> {
+        let repository = DatabaseRepositoryImpl::new(db);
+        block_on(repository.find(&id)).unwrap()
+    }
+
+    fn find_tag_by_name(db: Db, name: String) -> Option<Tag> {
+        let repository = DatabaseRepositoryImpl::new(db);
+        block_on(repository.find_by_name(name)).unwrap()
     }
 }
--- src-tauri/src/kernel/repository/tag.rs
+++ src-tauri/src/kernel/repository/tag.rs
@@ -8,4 +8,5 @@ use async_trait::async_trait;
 pub trait TagRepository {
     async fn find(&self, id: &Id<Tag>) -> anyhow::Result<Option<Tag>>;
     async fn insert(&self, source: NewTag) -> anyhow::Result<()>;
+    async fn find_by_name(&self, name: String) -> anyhow::Result<Option<Tag>>;
 }

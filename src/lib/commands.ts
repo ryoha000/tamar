bf72efbea67de1c73915e45040c582ef5eb8705b
ryoha <the7uya@gmail.com>
2022-08-22 09:02:25 +0900

    fix command

--- src/lib/commands.ts
+++ src/lib/commands.ts
@@ -1,10 +1,32 @@
 import { invoke } from "@tauri-apps/api";
 import { Usages } from "../components/TopPage/use/dirUsage";
 import { DirPathInfo } from "../components/TopPage/use/exploreDir";
+import { Work } from "./types";
 
 export const commandImportDirectory = async (
   dirPathInfos: DirPathInfo[],
   usages: Usages
 ) => {
   await invoke<null>("import_directory", { dirPathInfos, usages });
+};
+
+export const SORT_COLUMNS = ["updated_at", "title"] as const;
+export type SortColumnKind = typeof SORT_COLUMNS[number];
+
+export const commandSearchWork = async (payload: {
+  limit: number;
+  offset: number;
+  search: string;
+  tags: string[];
+  sortCol: string;
+  sortDesc: boolean;
+}) => {
+  return await invoke<Work[]>("search_work", {
+    limit: payload.limit,
+    offset: payload.offset,
+    search: payload.search,
+    tags: payload.tags,
+    sortCol: payload.sortCol,
+    sortDesc: payload.sortDesc,
+  });
 };

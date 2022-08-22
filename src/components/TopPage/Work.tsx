1552781e05d2a427f9a78bc86fda18b8c52f8aec
ryoha <the7uya@gmail.com>
2022-08-22 14:04:53 +0900

    fix

--- src/components/TopPage/Work.tsx
+++ src/components/TopPage/Work.tsx
@@ -12,19 +12,10 @@ const Work: Component<Props> = (props) => {
   const images = () => props.work.paths.map((v) => convertFileSrc(v));
 
   return (
-    <div class="flex flex-col items-center content-center gap-2 rounded hover:bg-secondary transition-all p-3 cursor-pointer">
+    <div class="flex flex-col items-center content-center gap-2 hover:scale-110 transition-all p-3 cursor-pointer">
       <Show when={images().length}>
         <img class="object-contain" src={images()[0]} />
       </Show>
-      {/* <div>{props.work.title}</div>
-      <div>{props.work.artist.name}</div>
-      <For each={props.work.tags}>
-        {(tag, i) => (
-          <div class="flex">
-            <Tag tag={tag} />
-          </div>
-        )}
-      </For> */}
     </div>
   );
 };

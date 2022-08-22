172cd93c3cdbd56f2f375adf17cdce140d76ddd9
ryoha <the7uya@gmail.com>
2022-08-22 12:47:12 +0900

    impl SearchInput filter title

--- src/components/TopPage/SearchInput.tsx
+++ src/components/TopPage/SearchInput.tsx
@@ -1,8 +1,15 @@
 import type { Component } from "solid-js";
 import { AiOutlineSearch } from "solid-icons/ai";
 import Tag from "../UI/Tag";
+import { useStore } from "../../lib/store";
 
 const SearchInput: Component = () => {
+  const store = useStore();
+  if (!store) {
+    return <div>loading</div>;
+  }
+
+  const { setSearchText } = store;
   return (
     <div class="flex items-center border-solid border-text border rounded-full px-3 py-1 gap-2 transition-all focus-within:border-accent flex-1">
       <AiOutlineSearch />
@@ -11,7 +18,12 @@ const SearchInput: Component = () => {
         isCloseIcon={true}
         close={() => {}}
       />
-      <input class="w-full focus:outline-none"></input>
+      <input
+        class="w-full focus:outline-none"
+        type="text"
+        // @ts-ignore
+        oninput={(e) => setSearchText(e.target.value)}
+      ></input>
     </div>
   );
 };

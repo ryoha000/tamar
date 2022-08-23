6166b0344f4c8fe5751dd0f5cfc4cdce517daa1f
ryoha <the7uya@gmail.com>
2022-08-23 12:29:59 +0900

    add index

--- src-tauri/src/migration.rs
+++ src-tauri/src/migration.rs
@@ -19,16 +19,12 @@ pub async fn migration() {
 }
 
 pub fn migration_sync(db: Db) {
-    println!("start migration");
-
     let pool = db.0.clone();
 
     let sqls = get_migration_sqls();
     for sql in sqls.iter() {
         block_on(sqlx::query(sql).execute(&*pool)).unwrap();
     }
-
-    println!("end migration");
 }
 
 fn get_migration_sqls() -> Vec<String> {
@@ -37,7 +33,8 @@ CREATE TABLE IF NOT EXISTS artist (
 	id varchar(36) primary key,
 	name varchar(255) not null UNIQUE,
 	created_at datetime not null,
-	updated_at datetime not null
+	updated_at datetime not null,
+    UNIQUE(name)
 );
     "
     .to_string();
@@ -54,12 +51,22 @@ CREATE TABLE IF NOT EXISTS work (
     "
     .to_string();
 
+    let work_title_index = "CREATE INDEX work_title_index ON work(title);".to_string();
+    let work_artist_id_index = "CREATE INDEX work_artist_id_index ON work(artist_id);".to_string();
+    let work_updated_at_index =
+        "CREATE INDEX work_updated_at_index ON work(updated_at);".to_string();
+    let work_artist_id_updated_at_index =
+        "CREATE INDEX work_artist_id_updated_at_index ON work(artist_id, updated_at);".to_string();
+    let work_title_updated_at_index =
+        "CREATE INDEX work_title_updated_at_index ON work(title, updated_at);".to_string();
+
     let tag = "
 CREATE TABLE IF NOT EXISTS tag (
 	id varchar(36) primary key,
 	name varchar(255) not null UNIQUE,
 	created_at datetime not null,
-	updated_at datetime not null
+	updated_at datetime not null,
+    UNIQUE(name)
 );
     "
     .to_string();
@@ -80,5 +87,16 @@ CREATE TABLE IF NOT EXISTS work_tag_map (
     INSERT OR IGNORE INTO artist(id, name, created_at, updated_at) VALUES(\"01GAYXAS9G6YHP4BTZDFT360P7\", \"{}\", datetime(CURRENT_TIMESTAMP), datetime(CURRENT_TIMESTAMP))
     ", UNKNOWN_ARTIST_NAME);
 
-    return vec![artist, work, tag, work_tag_map, insert_unknown_artist];
+    return vec![
+        artist,
+        work,
+        work_artist_id_index,
+        work_artist_id_updated_at_index,
+        work_title_index,
+        work_title_updated_at_index,
+        work_updated_at_index,
+        tag,
+        work_tag_map,
+        insert_unknown_artist,
+    ];
 }

92ed16a713c0810d7ce0cdac34bbdc3316e2c5ba
ryoha <the7uya@gmail.com>
2022-08-24 20:04:08 +0900

    impl MenuDialogTagList

--- src/components/WorkPage/MenuDialogTagList.tsx
+++ src/components/WorkPage/MenuDialogTagList.tsx
@@ -1,64 +1,77 @@
 import { AiOutlinePlus } from "solid-icons/ai";
+import { Component, createResource, createSignal, For, Show } from "solid-js";
 import {
-  Component,
-  createResource,
-  createSignal,
-  For,
-  onMount,
-} from "solid-js";
-import { commandGetTagSuggest, commandSelectTag } from "../../lib/commands";
+  commandAttachTagByName,
+  commandDetachTag,
+  commandGetTagSuggest,
+} from "../../lib/commands";
+import useInputList from "../../lib/inputList";
 import { Tag as TagI } from "../../lib/types";
 import Tag from "../UI/Tag";
 
 interface Props {
+  workId: string;
   tags: TagI[];
+  refetch: () => void;
 }
 
 const MenuDialogTagList: Component<Props> = (props) => {
-  const removeTag = (id: string) => {};
-
-  const [newTagText, setNewTagText] = createSignal("");
-  const onchange = (
-    e: Event & { currentTarget: HTMLInputElement; target: Element }
-  ) => {
-    setNewTagText(e.currentTarget.value);
+  const removeTag = async (id: string) => {
+    await commandDetachTag(props.workId, id);
+    props.refetch();
   };
 
-  const [suggests, { mutate }] = createResource(
-    newTagText,
-    commandGetTagSuggest,
-    { initialValue: [] }
-  );
-  onMount(async () => {
-    const initialSuggest = await commandSelectTag(50);
-    mutate(initialSuggest);
+  const [newTagText, setNewTagText] = createSignal("");
+
+  const inputCallback = (ele: HTMLInputElement) => {
+    setNewTagText(ele.value);
+  };
+  const listCallback = (ele: HTMLInputElement) => {
+    setNewTagText(ele.value);
+    attachTag(null);
+  };
+  const { input, keydown } = useInputList(inputCallback, listCallback);
+
+  const [suggests] = createResource(newTagText, commandGetTagSuggest, {
+    initialValue: [],
   });
 
+  const attachTag = async (e: Event | null) => {
+    e?.preventDefault();
+    await commandAttachTagByName(props.workId, newTagText());
+    props.refetch();
+    setNewTagText("");
+  };
+
   return (
-    <div>
+    <div class="flex flex-col gap-4">
       <div class="flex flex-wrap items-center gap-2 w-full">
         <For each={props.tags}>
-          {(tag, i) => (
-            <Tag tag={tag} isCloseIcon={true} close={() => removeTag(tag.id)} />
-          )}
+          {(tag, i) => <Tag tag={tag} close={() => removeTag(tag.id)} />}
         </For>
       </div>
-      <div class="w-full flex items-center gap-4">
+      <form onsubmit={attachTag} class="w-full flex items-center gap-4">
         <input
-          id="menudialogtaglist-input"
-          class="flex-1 border-b border-text focus:border-secondary transition-all"
+          list="menudialogtaglist"
+          type="text"
+          class="flex-1 border-b border-text focus:border-secondary transition-all max-w-xs"
           value={newTagText()}
-          onchange={onchange}
+          oninput={input}
+          onkeydown={keydown}
         />
-        <datalist id="menudialogtaglist-input">
+        <datalist id="menudialogtaglist">
           <For each={suggests()}>
-            {(suggest, i) => <option>{suggest.name}</option>}
+            {(suggest, i) => (
+              <Show when={!props.tags.find((v) => v.id === suggest.id)}>
+                <option>{suggest.name}</option>
+              </Show>
+            )}
           </For>
         </datalist>
-        <button>
+        <button type="submit">
           <AiOutlinePlus size="1.5rem" />
         </button>
-      </div>
+      </form>
     </div>
   );
 };

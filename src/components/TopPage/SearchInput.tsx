129f0c91cc35259c4365e88d3a415f330068a5ef
ryoha000 <the7uya@gmail.com>
2022-08-30 21:28:07 +0900

    show artist_name in SearchInput

--- src/components/TopPage/SearchInput.tsx
+++ src/components/TopPage/SearchInput.tsx
@@ -1,8 +1,11 @@
-import { Component, For } from "solid-js";
+import { Component, createResource, For, Show } from "solid-js";
 import { AiOutlineSearch } from "solid-icons/ai";
 import useSuggest, { UseSuggestProps } from "./use/suggest";
 import { Tag as TagI } from "../../lib/types";
 import SearchTagList from "./SearchTagList";
+import { useLocation, useNavigate, useParams } from "@solidjs/router";
+import { commandGetArtist } from "../../lib/commands";
+import Tag from "../UI/Tag";
 
 type Props = {
   tags: TagI[];
@@ -13,9 +16,30 @@ const SearchInput: Component<Props> = (props) => {
   const removeTag = (id: string) => {
     props.setTags((prev) => prev.filter((v) => v.id !== id));
   };
+
+  const location = useLocation();
+  const navigator = useNavigate();
+  const isArtistPage = () => location.pathname.startsWith("/artist");
+  const goHome = () => navigator("/");
+  const params = useParams();
+  const getArtist = async (id: string) => {
+    if (id === "") {
+      return null;
+    }
+    return await commandGetArtist(id);
+  };
+  const [artist] = createResource(
+    () => (isArtistPage() ? params["id"] : ""),
+    getArtist,
+    { initialValue: null }
+  );
+
   return (
     <div class="flex items-center flex-1 border-solid border-text border rounded-full px-3 py-1 gap-2 transition-all focus-within:border-accent">
       <AiOutlineSearch />
+      <Show when={isArtistPage() && artist()}>
+        <Tag tag={{ ...artist()! }} close={goHome} />
+      </Show>
       <SearchTagList tags={props.tags} removeTag={removeTag} />
       <input
         class="focus:outline-none flex-1"

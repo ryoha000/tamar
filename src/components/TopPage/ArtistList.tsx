d29bc80516af3355a0da0a9c1f8f12d0317db103
ryoha000 <the7uya@gmail.com>
2022-08-30 19:03:13 +0900

    use InfiniteScroll in ArtistList

--- src/components/TopPage/ArtistList.tsx
+++ src/components/TopPage/ArtistList.tsx
@@ -2,6 +2,7 @@ import { Component, createResource, Index } from "solid-js";
 import { useStore } from "../../lib/store";
 import { commandSearchArtist } from "../../lib/commands";
 import Artist from "../UI/Artist";
+import InfiniteScroll from "../UI/InfiniteScrollWrapper";
 
 const ArtistList: Component = () => {
   const store = useStore();
@@ -18,11 +19,19 @@ const ArtistList: Component = () => {
   );
 
   return (
-    <div class="flex flex-col items-center w-full gap-4">
-      <Index each={artists()}>
-        {(artist, i) => <Artist artist={artist()} refetch={refetch} />}
-      </Index>
-    </div>
+    <InfiniteScroll
+      command={commandSearchArtist}
+      mutate={mutate}
+      req={store.searchRequest()}
+      isInitialEmpty={artists().length === 0}
+      initOffset={store.searchRequest().offset}
+    >
+      <div class="flex flex-col items-center w-full gap-4">
+        <Index each={artists()}>
+          {(artist, i) => <Artist artist={artist()} refetch={refetch} />}
+        </Index>
+      </div>
+    </InfiniteScroll>
   );
 };
 

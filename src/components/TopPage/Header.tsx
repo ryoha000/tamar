d7cf4dfbea79feca325f4e5b79224b6d01e2e1c0
ryoha000 <the7uya@gmail.com>
2022-08-30 19:57:50 +0900

    rm comment

--- src/components/TopPage/Header.tsx
+++ src/components/TopPage/Header.tsx
@@ -1,5 +1,4 @@
 import { Component, Show } from "solid-js";
-import useOption from "../../lib/option";
 import { useStore } from "../../lib/store";
 import HeaderNextPrev from "../UI/HeaderNextPrev";
 import ByArtistToggle from "./ByArtistToggle";
@@ -14,7 +13,6 @@ const Header: Component = () => {
     <Show when={store}>
       <div class="fixed top-0 left-0 w-full z-header h-header px-4 py-2 bg-white">
         <div class="grid grid-cols-top-header items-center w-full gap-2">
-          {/* <div class="flex items-center w-full gap-2"> */}
           <HeaderNextPrev />
           <SearchInput
             setText={store!.setSearchText}

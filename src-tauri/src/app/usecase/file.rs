b6bd0e683fa94c51d8b4a5d63a2ea865e3e6ada4
ryoha000 <the7uya@gmail.com>
2022-09-06 00:36:35 +0900

    impl file_use_case

--- src-tauri/src/app/usecase/file.rs
+++ src-tauri/src/app/usecase/file.rs
@@ -17,6 +17,14 @@ impl<R: RepositoriesModuleExt> FileUseCase<R> {
     pub fn get_work_paths(&self, id: &Id<Work>) -> anyhow::Result<Vec<String>> {
         self.repositories.file_repository().get_work_paths(id)
     }
+    pub fn get_file_name(&self, path_str: &str) -> anyhow::Result<String> {
+        self.repositories.file_repository().get_file_name(path_str)
+    }
+    pub fn extract_zip_file(&self, file_path_str: &str, dir_path_str: &str) -> anyhow::Result<()> {
+        self.repositories
+            .file_repository()
+            .extract_zip_file(file_path_str, dir_path_str)
+    }
     pub fn save_original_files(&self, source: SaveOriginalFiles) -> anyhow::Result<()> {
         self.repositories.file_repository().save_work_files(source)
     }
@@ -27,5 +35,8 @@ impl<R: RepositoriesModuleExt> FileUseCase<R> {
     }
     pub fn delete_work_file(&self, file: String) -> anyhow::Result<()> {
         self.repositories.file_repository().delete_file(file)
+    }
+    pub fn delete_dir(&self, dir: String) -> anyhow::Result<()> {
+        self.repositories.file_repository().delete_dir(dir)
     }
 }

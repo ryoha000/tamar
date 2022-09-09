4833013320e3f1e48a80223d44a83867ba44c379
ryoha000 <the7uya@gmail.com>
2022-09-10 00:17:26 +0900

    import_directory to concurrent

--- src-tauri/src/driver/command/import_directory.rs
+++ src-tauri/src/driver/command/import_directory.rs
@@ -4,7 +4,7 @@ use tauri::{State, Window};
 use crate::{
     app::model::{
         artist::{CreateArtist, GetByNameArtist},
-        file::SaveOriginalFiles,
+        file::{SaveOriginalFiles, SaveThumbnails},
         tag::{CreateTag, GetByNameTag},
         work::{GetByTitleWork, ImportWork},
         work_tag_map::CreateWorkTagMap,
@@ -12,6 +12,7 @@ use crate::{
     driver::context::errors::CommandError,
     driver::module::Modules,
     driver::{model::import_directory::*, module::ModulesExt},
+    kernel::model::{work::Work, Id},
     migration::UNKNOWN_ARTIST_NAME,
 };
 
@@ -32,16 +33,16 @@ pub async fn import_directory(
     let mut tag_usage_map = HashMap::new(); // tag は複数追加可能だから型が違う
 
     // usages の validate
-    for each_path_usage in usages.iter() {
+    for each_path_usage in usages.into_iter() {
         let mut is_title_exist = false;
-        for each_deps_usage in each_path_usage.1.iter() {
-            match &**(each_deps_usage.1) {
+        for each_deps_usage in each_path_usage.1.into_iter() {
+            match &*(each_deps_usage.1) {
                 "タグ" => {
-                    if !tag_usage_map.contains_key(each_path_usage.0) {
+                    if !tag_usage_map.contains_key(&each_path_usage.0) {
                         tag_usage_map.insert(each_path_usage.0, vec![]);
                     }
                     tag_usage_map
-                        .get_mut(each_path_usage.0)
+                        .get_mut(&each_path_usage.0)
                         .unwrap()
                         .push(each_deps_usage.0);
                 }
@@ -81,9 +82,18 @@ pub async fn import_directory(
         }
     }
 
-    for dir_path_info in dir_path_infos.iter() {
+    let artist_usage_map = Arc::new(artist_usage_map);
+    let title_usage_map = Arc::new(title_usage_map);
+    let tag_usage_map = Arc::new(tag_usage_map); // tag は複数追加可能だから型が違う
+    let window_arc = Arc::new(window);
+
+    let mut features = vec![];
+    for dir_path_info in dir_path_infos.into_iter() {
+        let m = modules.inner().clone();
+        let w = window_arc.clone();
+
         let max_deps = &(dir_path_info.dir_deps.len() as u16);
-        let get_name = |deps: &&u16| dir_path_info.dir_deps[(**deps - 1) as usize].name.clone();
+        let get_name = |deps: &u16| dir_path_info.dir_deps[(*deps - 1) as usize].name.clone();
 
         // -------- artist に関係する処理 ここから ---------
         // artist を insert
@@ -92,8 +102,7 @@ pub async fn import_directory(
             Some(deps) => {
                 artist_name = get_name(deps);
 
-                modules
-                    .artist_use_case()
+                m.artist_use_case()
                     .register_artist(CreateArtist::new(artist_name.clone()))
                     .await?;
             }
@@ -101,7 +110,7 @@ pub async fn import_directory(
         }
 
         // work の insert に使うため insert したはずの artist を取得
-        let artist = modules
+        let artist = m
             .artist_use_case()
             .find_artist_by_name(GetByNameArtist::new(artist_name.clone()))
             .await?;
@@ -126,8 +135,7 @@ pub async fn import_directory(
             }
         }
         // work の insert
-        modules
-            .work_use_case()
+        m.work_use_case()
             .import_work(ImportWork::new(
                 work_title.clone(),
                 artist.id.clone(),
@@ -136,7 +144,7 @@ pub async fn import_directory(
             .await?;
 
         // tag の insert に使うため insert したはずの work を取得
-        let work = modules
+        let work = m
             .work_use_case()
             .get_by_title_work(GetByTitleWork::new(work_title.clone(), artist.id.clone()))
             .await?;
@@ -149,17 +157,6 @@ pub async fn import_directory(
         let work = work.unwrap();
         // -------- work に関係する処理 ここまで ---------
 
-        // ファイルコピー
-        modules
-            .file_use_case()
-            .save_original_files(SaveOriginalFiles::new(
-                work.id.clone(),
-                dir_path_info.path.clone(),
-            ))?;
-        if let Err(e) = window.emit("import_dir_progress", ProgressPayload { count: 1 }) {
-            return Err(CommandError::Anyhow(anyhow::anyhow!(e)));
-        }
-
         // -------- tag に関係する処理 ここから ---------
         match tag_usage_map.get(max_deps) {
             // tag をつけるとき
@@ -168,13 +165,12 @@ pub async fn import_directory(
                     let tag_name = get_name(deps);
 
                     // tag を insert
-                    modules
-                        .tag_use_case()
+                    m.tag_use_case()
                         .register_tag(CreateTag::new(tag_name.clone()))
                         .await?;
 
                     // work_tag_map を作る必要があるため insert したはずの tag を取得
-                    let tag = modules
+                    let tag = m
                         .tag_use_case()
                         .find_tag_by_name(GetByNameTag::new(tag_name))
                         .await?;
@@ -186,15 +182,50 @@ pub async fn import_directory(
                     let tag = tag.unwrap();
 
                     // work_tag_map を insert
-                    modules
-                        .work_tag_map_use_case()
+                    m.work_tag_map_use_case()
                         .register_work_tag_map(CreateWorkTagMap::new(work.id.clone(), tag.id))
                         .await?
                 }
             }
             None => {} // tag をつけなくていいため何もしない
         }
-        // -------- tag に関係する処理 ここまで ---------
+
+        let handle = tauri::async_runtime::spawn(async move {
+            import_individual(m, w, &dir_path_info, work.id.clone())
+        });
+        features.push(handle);
+    }
+    println!("end insert");
+
+    for f in features {
+        let join_res = f.await;
+        if let Err(e) = join_res {
+            return Err(CommandError::Anyhow(anyhow::anyhow!(
+                "{:#?} (internal error)",
+                e
+            )));
+        } else {
+            join_res.unwrap()?;
+        }
+    }
+
+    Ok(())
+}
+
+fn import_individual(
+    m: Arc<Modules>,
+    window: Arc<Window>,
+    dir_path_info: &DirPathInfo,
+    id: Id<Work>,
+) -> anyhow::Result<(), CommandError> {
+    // ファイルコピー
+    m.file_use_case()
+        .save_original_files(SaveOriginalFiles::new(
+            id.clone(),
+            dir_path_info.path.clone(),
+        ))?;
+    if let Err(e) = window.emit("import_dir_progress", ProgressPayload { count: 1 }) {
+        return Err(CommandError::Anyhow(anyhow::anyhow!(e)));
     }
 
     Ok(())

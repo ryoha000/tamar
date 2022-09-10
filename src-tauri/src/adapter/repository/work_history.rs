ca01733dccb1b9f8e0bcd76e762d93f59091becd
ryoha000 <the7uya@gmail.com>
2022-09-11 07:52:41 +0900

    impl find_latest work_history

--- src-tauri/src/adapter/repository/work_history.rs
+++ src-tauri/src/adapter/repository/work_history.rs
@@ -1,4 +1,5 @@
 use crate::adapter::model::work_history::WorkHistoryTable;
+use crate::kernel::model::work::Work;
 use crate::kernel::model::{
     work_history::{NewWorkHistory, WorkHistory},
     Id,
@@ -19,6 +20,21 @@ impl WorkHistoryRepository for DatabaseRepositoryImpl<WorkHistory> {
                 .fetch_one(&*pool)
                 .await
                 .ok();
+        match work_history_table {
+            Some(st) => Ok(Some(st.try_into()?)),
+            None => Ok(None),
+        }
+    }
+
+    async fn find_latest(&self, id: &Id<Work>) -> anyhow::Result<Option<WorkHistory>> {
+        let pool = self.pool.0.clone();
+        let work_history_table = query_as::<_, WorkHistoryTable>(
+            "select * from work_history where work_id = ? ORDER BY updated_at DESC",
+        )
+        .bind(id.value.to_string())
+        .fetch_one(&*pool)
+        .await
+        .ok();
         match work_history_table {
             Some(st) => Ok(Some(st.try_into()?)),
             None => Ok(None),
@@ -87,6 +103,43 @@ mod test {
         assert_eq!(found.id.value, id);
     }
 
+    #[test]
+    fn test_find_latest_work_history() {
+        let db = get_test_db();
+        let artist_id = Ulid::new();
+        let work_id = Ulid::new();
+        let name = random_string();
+
+        insert_artist(
+            db.clone(),
+            NewArtist::new(Id::new(artist_id), name.to_string()),
+        );
+        insert_work(
+            db.clone(),
+            NewWork::new(Id::new(work_id), random_string(), Id::new(artist_id)),
+        );
+
+        let id = Ulid::new();
+        insert_work_history(
+            db.clone(),
+            NewWorkHistory {
+                id: Id::new(id),
+                work_id: Id::new(work_id),
+            },
+        );
+        let id = Ulid::new();
+        insert_work_history(
+            db.clone(),
+            NewWorkHistory {
+                id: Id::new(id),
+                work_id: Id::new(work_id),
+            },
+        );
+        let found = find_latest_work_history(db, Id::new(work_id)).unwrap();
+
+        assert_eq!(found.id.value, id);
+    }
+
     fn insert_artist(db: Db, new_artist: NewArtist) {
         let repository = DatabaseRepositoryImpl::<Artist>::new(db);
         block_on(repository.insert(new_artist)).unwrap()
@@ -105,5 +158,10 @@ mod test {
     fn find_work_history(db: Db, id: Id<WorkHistory>) -> Option<WorkHistory> {
         let repository = DatabaseRepositoryImpl::<WorkHistory>::new(db);
         block_on(repository.find(&id)).unwrap()
+    }
+
+    fn find_latest_work_history(db: Db, id: Id<Work>) -> Option<WorkHistory> {
+        let repository = DatabaseRepositoryImpl::<WorkHistory>::new(db);
+        block_on(repository.find_latest(&id)).unwrap()
     }
 }
--- src-tauri/src/kernel/repository/work_history.rs
+++ src-tauri/src/kernel/repository/work_history.rs
@@ -1,4 +1,5 @@
 use crate::kernel::model::{
+    work::Work,
     work_history::{NewWorkHistory, WorkHistory},
     Id,
 };
@@ -7,5 +8,6 @@ use async_trait::async_trait;
 #[async_trait]
 pub trait WorkHistoryRepository {
     async fn find(&self, id: &Id<WorkHistory>) -> anyhow::Result<Option<WorkHistory>>;
+    async fn find_latest(&self, id: &Id<Work>) -> anyhow::Result<Option<WorkHistory>>;
     async fn insert(&self, source: NewWorkHistory) -> anyhow::Result<()>;
 }
